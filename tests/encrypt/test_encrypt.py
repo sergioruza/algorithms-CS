2400cd91d6e20ca67e8a117327a0fe83f31d6377
Sergio <sergioruza22@gmail.com>
2023-05-25 18:25:09 -0300

    feat: test_encrypt_message

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,11 +1,15 @@
 from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
     message = "Hello, World!"
-    key = 4
-    expected_result = "!dlroW ,o_lleH"
-    assert encrypt_message(message, key) == expected_result
+    assert encrypt_message(message, 4) == "!dlroW ,o_lleH"
+    assert encrypt_message(message, 3) == "leH_!dlroW ,ol"
+    assert encrypt_message(message, 50) == "!dlroW ,olleH"
+
+    with pytest.raises(TypeError):
+        encrypt_message(message, "44")
 
 
-# print(encrypt_message("hello world", 5))
+print(encrypt_message("hello world", 50))
