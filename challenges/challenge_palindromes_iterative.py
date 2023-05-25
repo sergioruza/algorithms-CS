7d17b8aa17e7c7d74ca306b836600ad8043ba1ce
Sergio <sergioruza22@gmail.com>
2023-05-25 18:09:06 -0300

    feat: is_palindrome_iterative

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,11 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if word == "":
+        return False
+    palindrome = word.replace(" ", "").lower()
+
+    invert = palindrome[::-1]
+
+    if word.lower() == invert:
+        return True
+    else:
+        return False
