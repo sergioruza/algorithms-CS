469de9b1c8ac0d8a68981440ef7b43583078d710
Sergio <sergioruza22@gmail.com>
2023-05-25 17:43:22 -0300

    feat: test_encrypt_message

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,35 +1,35 @@
-def is_anagram(first_string, second_string):
-    if (
-        len(first_string) != len(second_string)
-        or first_string == ""
-        or second_string == ""
-    ):
-        return False
+# def is_anagram(first_string, second_string):
+#     if (
+#         len(first_string) != len(second_string)
+#         or first_string == ""
+#         or second_string == ""
+#     ):
+#         return False
 
-    first_string = list(first_string.lower())
-    second_string = list(second_string.lower())
+#     first_string = list(first_string.lower())
+#     second_string = list(second_string.lower())
 
-    for index in range(len(first_string)):
-        for i in range(len(first_string) - index - 1):
-            if first_string[i] > first_string[i + 1]:
-                first_string[i], first_string[i + 1] = (
-                    first_string[i + 1],
-                    first_string[i],
-                )
+#     for index in range(len(first_string)):
+#         for i in range(len(first_string) - index - 1):
+#             if first_string[i] > first_string[i + 1]:
+#                 first_string[i], first_string[i + 1] = (
+#                     first_string[i + 1],
+#                     first_string[i],
+#                 )
 
-    for index in range(len(second_string)):
-        for i in range(len(second_string) - index - 1):
-            if second_string[i] > second_string[i + 1]:
-                second_string[i], second_string[i + 1] = (
-                    second_string[i + 1],
-                    second_string[i],
-                )
+#     for index in range(len(second_string)):
+#         for i in range(len(second_string) - index - 1):
+#             if second_string[i] > second_string[i + 1]:
+#                 second_string[i], second_string[i + 1] = (
+#                     second_string[i + 1],
+#                     second_string[i],
+#                 )
 
-    first_word_sort = "".join(first_string)
-    second_word_sort = "".join(second_string)
-    result = (
-        first_word_sort,
-        second_word_sort,
-        first_word_sort == second_word_sort,
-    )
-    return result
+#     first_word_sort = "".join(first_string)
+#     second_word_sort = "".join(second_string)
+#     result = (
+#         first_word_sort,
+#         second_word_sort,
+#         first_word_sort == second_word_sort,
+#     )
+#     return result
--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,11 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    message = "Hello, World!"
+    key = 4
+    expected_result = "!dlroW ,o_lleH"
+    assert encrypt_message(message, key) == expected_result
+
+
+# print(encrypt_message("hello world", 5))
