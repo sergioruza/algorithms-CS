f6101fd96dc11fde9a15d998b3ca315977b243bb
Sergio <sergioruza22@gmail.com>
2023-05-25 14:57:02 -0300

    feat: study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,19 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not target_time or not permanence_period:
+        return None
+
+    count = 0
+
+    for period in permanence_period:
+        if (
+            None in period
+            or isinstance(period[0], str)
+            or isinstance(period[1], str)
+        ):
+            count = None
+            break
+
+        if period[0] <= target_time <= period[1]:
+            count += 1
+
+    return count
